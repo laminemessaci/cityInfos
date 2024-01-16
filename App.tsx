36561c15b80db048e39c23dbae1b0fc9be64d38f
lamminemessaci <lamine.messaci18@gmail.com>
2024-01-17 00:03:29 +0100

    Done

--- App.tsx
+++ App.tsx
@@ -1,24 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { useTheme } from './src/theme/ThemeProvider';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import Header from "./src/components/Header";
+import { useTheme } from "./src/theme/ThemeProvider";
 
 export default function App() {
-
-  const theme = useTheme()
-  console.log(theme)
+  const theme = useTheme();
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Header />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
