6fec5e5a8fd284e5cdd46363a38ac132b06108a0
lamminemessaci <lamine.messaci18@gmail.com>
2024-01-17 04:00:42 +0100

    fix style

--- App.tsx
+++ App.tsx
@@ -5,22 +5,13 @@ import { useTheme } from "./src/theme/ThemeProvider";
 import { Provider } from "react-redux";
 import { store } from "./src/redux/store";
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-const Stack = createStackNavigator();
-
 export default function App() {
   const theme = useTheme();
 
   return (
     <Provider store={store}>
       <Header />
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen}  />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <HomeScreen />
     </Provider>
   );
 }
